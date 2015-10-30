81f52e39a36c135c08f55834ce62a6be839cff64
WilsonWangTHU <wode406@hotmail.com>
2015-10-30 16:14:17 +0800

    Proj1 finished

--- proj1/banker.cpp
+++ proj1/banker.cpp
@@ -9,40 +9,51 @@
 
 
 #include <iostream>
-#include <pthread.h>
 #include <fstream>
+#include <sstream>
+#include <pthread.h>
+#include <unistd.h>
+#include <string>
 #include <stdio.h>
 #include <semaphore.h>
 #include <time.h>       /* clock_t, clock, CLOCKS_PER_SEC */
 #include <vector>
+#include <deque>
 
-static const int NUM_COUNTER = 7;
+static const int NUM_COUNTER = 2;
 static const float startTime = (float)clock()/CLOCKS_PER_SEC;
 
 class customer;
 class banker;
-
-void* customerBehavior(void* ptr);
-void* bankerBehavior(void* ptr);
 
 class queueingLine {
     public:
         queueingLine() {
             size = 0; 
             servingTime.clear();
-            sem_init(&queueWriting, 0, 0);
+            sem_init(&queueNum, 0, 0);
             sem_init(&accessQueue, 0, 1);
+            count = 0;
+            servedCount = 0;
         }
         ~queueingLine() {
             servingTime.clear();
-            sem_destroy(&queueWriting);
+            sem_destroy(&queueNum);
+            sem_destroy(&accessQueue);
         }
-        void serve();
-        void add(customer);
+        void serve(banker* Me);
+        void add(customer Me);
+        int getServingTime() {
+            int time = servingTime[0];
+            servingTime.pop_front();
+            return time;
+        }
     private:
-        std::vector<int> servingTime;
+        std::deque<int> servingTime;
+        int count;
+        int servedCount;
         int size;
-        sem_t queueWriting;
+        sem_t queueNum;
         sem_t accessQueue;
 };
 
@@ -52,40 +63,107 @@ class customer{
             servingTime = serveTime;
             startTime = serveStartTime;
         }
-        customer();
         bool inline isActivate() {
             return (float)clock()/CLOCKS_PER_SEC > startTime;
         }
+        void customerSleep() {  
+            /* the customer arrives after sleep */
+            sleep((unsigned)startTime);
+        }
+        static void* customerBehavior(void* ptr);
+        int getServingTime() { return servingTime; }
     private:
-        int servingTime;
-        int startTime;
+        float servingTime;
+        float startTime;
 };
 
 class banker{
     public:
-        banker() { servingTime = 0; startTime = 0; }
+        banker(int Id) { 
+            servingTime = 0;
+            startTime = 0; 
+            counterId = Id;
+        }
         bool inline isActivate() {
             return (float)clock()/CLOCKS_PER_SEC >
                 startTime + servingTime;
         }
+        static void* bankerBehavior(void* ptr);
+        void serve(int Who) {
+            std::cout << "The customer " << Who <<
+                " is served by counter " << counterId; 
+            std::cout << ". He takes " << servingTime 
+                << " seconds" << std::endl;
+                
+            sleep((unsigned)servingTime);
+        }
+        void setServTime(int newTime) { servingTime = newTime; }
     private:
         int servingTime;
         int startTime;
+        int counterId;
 };
+
+void queueingLine::add(customer Me) {
+    sem_wait(&accessQueue);
+    count = count + 1;  //
+    servingTime.push_back(Me.getServingTime());
+    std::cout << "A customer step in side, at time: " <<
+        (float)clock()/CLOCKS_PER_SEC << std::endl;
+    std::cout << "  He get the num:" << count << std::endl << std::endl;
+    sem_post(&queueNum);  // add one to the line
+    sem_post(&accessQueue);
+};
+
+void queueingLine::serve(banker* Me) {
+    sem_wait(&queueNum);
+    sem_wait(&accessQueue);
+    servedCount = servedCount + 1;
+    int serveTime = getServingTime();
+    int serveWho = servedCount;
+    sem_post(&accessQueue);
+
+    Me->setServTime(serveTime);
+    Me->serve(serveWho);
+}
+
+
+queueingLine bankLine;  // this is the banker line
 
 int main(int argc, char** argv){
     // in this part, read the data
-    int numCtm = 10;
+    int numCtm = 0;
+    std::ifstream inputFile(argv[1]);
+    std::string line;
+
+    std::vector<banker> counter;
+    std::vector<customer> buyers;
+
+    while(std::getline(inputFile, line)) {
+        std::istringstream iss(line);
+        int tempId, tempArrive, tempServe;
+        if (!(iss >> tempId >> tempArrive  >> tempServe)) { break; } // error
+        numCtm ++;
+
+        customer tempCustomer((tempServe),
+                (tempArrive));
+        buyers.push_back(tempCustomer);
+    }
+
+    for (int i_banker = 0; i_banker < NUM_COUNTER; i_banker ++) {
+        banker tempBanker(i_banker);
+        counter.push_back(tempBanker);
+    }
+
+    std::cout << "The thread initilizing!" << std::endl;
 
     pthread_t* customerThread = new pthread_t[numCtm];
     pthread_t* counterThread = new pthread_t[NUM_COUNTER];
-    banker* counter = new banker[NUM_COUNTER];
-    customer* buyers = new customer[numCtm]();
 
     for (int i_thread = 0; i_thread < numCtm; i_thread ++) {
         pthread_create(
                 &(customerThread[i_thread]),
-                NULL, customerBehavior, 
+                NULL, customer::customerBehavior, 
                 (void*)(&buyers[i_thread])
                 );
     }
@@ -93,22 +171,31 @@ int main(int argc, char** argv){
     for (int i_thread = 0; i_thread < NUM_COUNTER; i_thread ++) {
         pthread_create(
                 &(counterThread[i_thread]),
-                NULL, bankerBehavior, 
+                NULL, banker::bankerBehavior, 
                 (void*)(&counter[i_thread])
                 );
     }
 
-    sem_t sem_id;
-    sem_init(&sem_id, 0, 7);
+    std::cout << "  The thread initilized!" << std::endl;
+    for(;;){}
     //sem_getvalue(&sem_id, &temp);
     //std::cout<<temp<<std::endl;
+    delete [] customerThread;
+    delete [] counterThread;
     return 0;
 }
 
-void* customerBehavior(void* ptr) {
-
+void* customer::customerBehavior(void* ptr) {
+    customer* Me = (customer*) ptr;
+    Me->customerSleep();
+    bankLine.add(*Me);
+    return 0;
 };
 
-void* bankerBehavior(void* ptr) {
-
+void* banker::bankerBehavior(void* ptr) {
+    banker* Me = (banker*) ptr;
+    for(;;) {  // bankers work forever
+        bankLine.serve(Me);
+    }
+    return 0;
 };
--- proj1/make.sh
+++ proj1/make.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 g++ banker.cpp -lpthread -o banker.out
+
+chmod a+x banker.out
